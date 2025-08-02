b66aaa5cf5d0f73eb2da34a7db4a48602ee6e8e3
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2025-08-02 14:21:55 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
-  <footer class="bg-dark text-white text-center py-3 mt-5">
+  <footer class="bg-dark text-white text-center py-4 mt-5">
     <div class="container">
-      <small>&copy; 2025 Rayival Dede Saputra. Dibuat dengan Nuxt 3 & Bootstrap.</small>
+      <p class="mb-1">
+        &copy; 2025 Rayival Dede Saputra. All rights reserved.
+      </p>
+      <!-- <small class="text-secondary">Website ini dibangun dengan oleh Rayival</small> -->
     </div>
   </footer>
 </template>
