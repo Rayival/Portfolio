09baf54a22080dcdb21483365a242fa1d5849200
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2025-08-02 14:23:41 +0700

    Add files via upload

---
+++ plugins/aos.client.ts
@@ -0,0 +1,12 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export default defineNuxtPlugin(() => {
+  onMounted(() => {
+    AOS.init({
+      once: true,
+      duration: 800,
+      easing: 'ease-in-out',
+    })
+  })
+})
