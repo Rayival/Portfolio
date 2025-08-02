7ca4640dc8b2b7b55cf3a3cf27c64c1189d5e286
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2025-08-02 14:25:51 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,38 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
-  css: ['bootstrap/dist/css/bootstrap.min.css',
-        "bootstrap-icons/font/bootstrap-icons.css"
+  css: [
+    '@/assets/css/main.css',
+    'bootstrap/dist/css/bootstrap.min.css',
+    'bootstrap-icons/font/bootstrap-icons.css',
+    '@/assets/css/animate.css',
   ],
+
+  plugins: [
+    { src: '~/plugins/bootstrap.client.ts', mode: 'client' },
+    { src: '~/plugins/aos.client.ts', mode: 'client' },
+  ],
+
+  modules: [
+    '@nuxtjs/color-mode'
+  ],
+
+  colorMode: {
+    classSuffix: '',     
+    preference: 'system', 
+    fallback: 'light',    
+    storageKey: 'nuxt-color-mode' 
+  },
+
   app: {
-    head:{
-      script: [{
-        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2/dist/js/bootstrap.bundle.min.js'
-      }]
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',
+          defer: true,
+        },
+      ]
     }
   },
+
+  compatibilityDate: '2025-08-02'
 })
